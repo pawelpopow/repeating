b9fcf8aebe4cc3f8b62036e513621253d32e445c
ppopow <pawel.popow@gmail.com>
2022-08-15 12:22:57 +0100

    new five commit

---
+++ class.py
@@ -0,0 +1,5 @@
+class People:
+    def __init__(self, name, birthday, pesel):
+
+
+if __name__ == '__main__':
---
+++ new.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    number = 30
+    if number == 25:
+        print("Wartosc zmiennej to dwadzieścia pięć")
+    else:
+        if number == 29:
+            print("Wartosc zmiennej to dwadziescia dziewiec")
+        else:
+            print("Wartosc zmiennej nie jest ani dwadzieścia pięć ani dwadziescia dziewiec")
