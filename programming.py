748bec1a1e4d40810ae5c8bd0db85ecd50bc4229
ppopow <pawel.popow@gmail.com>
2022-08-16 13:22:44 +0100

    new 10 commit

--- programming.py
+++ programming.py
@@ -1,9 +1,13 @@
 def add_posfix(strg):
-    return str(strg) + '_postfix'
+    # return str(strg) + '_postfix'
+    # return str(strg) + '_next'
+    return str(strg) + '_communication'
 
 
 def mod_lista(lista):
-    return str(lista[0]) + "_" + str(lista[1][1])
+    # return str(lista[0]) + "_" + str(lista[1][1])
+    # return str(lista[0]) + " " + str(lista[1][1])
+    return str(lista[0]) + "," + str(lista[1][1])
 
 
 if __name__ == '__main__':
